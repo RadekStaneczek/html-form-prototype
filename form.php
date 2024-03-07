9a5f07904c8b9531b7336fe44a25a132f7020e86
Radek <95048257+RadekStaneczek@users.noreply.github.com>
2024-03-07 13:13:24 +0100

    Add files via upload

--- form.php
+++ form.php
@@ -1,20 +1,39 @@
 <?php
+
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
+$database = "piekarnia_staneczek";
+
+$name = $_POST["name"];
+$surname = $_POST["surname"];
+$phone = $_POST["phone_number"];
+$function = $_POST["function"];
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password,$database);
+$sql = "INSERT INTO pracownicy(id_pracownika,imie, nazwisko, numer_telefonu,funkcja) VALUES ('".$name."','".$surname."','".$phone."','".$function."')";
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 else{
-    echo "Connected successfully";
+  echo "Connected successfully";
+}
+if(isset($name))
+{
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+}
+else
+{
+  echo "brak";
 }
 
-$name
-
+$conn ->close();
 
 ?>
