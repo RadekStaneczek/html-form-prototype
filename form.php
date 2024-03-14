932f2cd8e76751ca9b29fb2cc940e73bd070a52f
Radek <95048257+RadekStaneczek@users.noreply.github.com>
2024-03-14 13:04:04 +0100

    Add files via upload

--- form.php
+++ form.php
@@ -5,23 +5,62 @@ $username = "root";
 $password = "";
 $database = "piekarnia_staneczek";
 
-$name = $_POST["name"];
-$surname = $_POST["surname"];
-$phone = $_POST["phone_number"];
-$function = $_POST["function"];
+$table = $_POST["table"];
 
-// Create connection
+$keys = array_keys($_POST);
+$id = [];
+
 $conn = new mysqli($servername, $username, $password,$database);
-$sql = "INSERT INTO pracownicy(id_pracownika,imie, nazwisko, numer_telefonu,funkcja) VALUES ('".$name."','".$surname."','".$phone."','".$function."')";
 
+$sql_ = "select * from dostawcy";
+if(true)
+{
+    $result = $conn->query($sql_);
+    $keys = array_keys($result -> fetch_assoc());
+    foreach($keys as $key => $value)
+    {
+        $id[$key] = $value;
+    }
+}
+$sql = "INSERT INTO".$table."(";
+$count = 0;
+foreach($keys as $key => $value)
+{
+    if($count==0)
+    {
+      $sql = $sql."'".$value."'";
+      $count +=1;
+    }
+    else
+    {
+      $sql = $sql.",'".$value."'";
+    }
+}
+$sql = $sql.") VALUES (";
+
+foreach($_POST as $key => $value)
+{
+  echo $key;
+  if(strpos($key,'id') === true)
+  {
+    $sql = $sql."'NULL'";
+  }
+  else
+  {
+    $sql = $sql.",'".$value."'";
+  }
+}
+$sql = $sql.");";
 // Check connection
-if ($conn->connect_error) {
+if ($conn->connect_error) 
+{
   die("Connection failed: " . $conn->connect_error);
 }
-else{
+else
+{
   echo "Connected successfully";
 }
-if(isset($name))
+if(true)
 {
   if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
@@ -35,5 +74,4 @@ else
 }
 
 $conn ->close();
-
 ?>
