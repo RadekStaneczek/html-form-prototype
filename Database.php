4260577ea5021ce30562c9e812a43ceb51d3ff69
Radek <95048257+RadekStaneczek@users.noreply.github.com>
2024-04-04 13:18:55 +0200

    Add files via upload

---
+++ Database.php
@@ -0,0 +1,26 @@
+<?php 
+    class Database
+    {
+        private $server;
+        private $username;
+        private $password;
+        private $database;
+        private $tables = [];
+
+        private $conn;
+        function __construct($server,$username,$password,$database,$tables)
+        {
+            $this->server = $server;
+            $this->username = $username;
+            $this->password = $password;
+            $this->database = $database;
+            $this->tables = $tables;
+
+            $this->conn = new mysqli($this->server, $this->username, $this->password, $this->database);
+        }
+        function DisplayTables()
+        {
+        }
+        
+    }
+?>
